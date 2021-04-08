ca8b44ef10d1206c028a84f4089fa5b0582cd7c4
Tabitha Godsey <trgodsey@cincinnatistate.edu>
2021-04-08 19:04:28 -0400

    Added Modals for Pet Type, Breed, Job Title, Roles, Service Types, Medication Methods, Users. Added SQL changes to Pet Image per request from Neina.

---
+++ test/Models/ServiceType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace test.Models {
+
+	public class ServiceType {
+
+
+		public int intServiceTypeID { get; set; }
+
+		[Required(ErrorMessage = "Service Type is Required!")]
+		[Display(Name = "Service Type")]
+		public string strServiceType { get; set; }
+	}
+}
